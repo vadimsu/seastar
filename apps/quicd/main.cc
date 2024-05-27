f580834d434333584e6b4c353acf5784fe37c610
vadimsu <vadim.suraev@gmail.com>
2024-05-27 12:41:12 -0700

    added libraries to link with

--- apps/quicd/main.cc
+++ apps/quicd/main.cc
@@ -4,6 +4,7 @@
 #include <seastar/core/thread.hh>
 #include <seastar/core/app-template.hh>
 #include <boost/program_options/option.hpp>
+#include "lsquic.h"
 
 namespace bpo = boost::program_options;
 
@@ -14,7 +15,9 @@ int main(int ac, char** av) {
 
     return app.run(ac, av, [&] {
         return seastar::async([&] {
-	    fmt::print("{} {}\n",__FILE__,__LINE__);
+	    fmt::print("{} {}\n",__FILE__,__LINE__); 
+	    auto rc = lsquic_global_init (LSQUIC_GLOBAL_SERVER);
+	    fmt::print("{} {} {}\n",__FILE__,__LINE__,rc);
             return 0;
         });
     });
